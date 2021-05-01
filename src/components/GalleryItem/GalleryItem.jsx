5d6d3bdefb197a582f3dce0e0644ade8a5f6d972
johnturner4004 <johnturner4004@gmail.com>
2021-05-01 09:02:55 -0400

    apply styling to GalleryItem to load like button in consistent place after each click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,8 @@
-import {useState} from 'react';
-import axios from 'axios';
-import './GalleryItem.css';
+import { useState } from "react";
+import axios from "axios";
+import "./GalleryItem.css";
 
-function GalleryItem( props ) {
+function GalleryItem(props) {
   // places imageData from props into a variable
   let imageData = props.imageData;
 
@@ -13,31 +13,43 @@ function GalleryItem( props ) {
 
   // function used to record clicks from the like button and send it to the server
   const incrementLikes = () => {
-    console.log('click', imageData.id);
-    axios.put(`/gallery/like/${imageData.id}`)
-    .then(response => {
-      console.log('Increased likes for image', response);
-    })
-    .catch(error => {
-      console.log('Error increasing likes');
-      alert('Unable ton increase like count');
-    });
-  }
+    console.log("click", imageData.id);
+    axios
+      .put(`/gallery/like/${imageData.id}`)
+      .then((response) => {
+        console.log("Increased likes for image", response);
+      })
+      .catch((error) => {
+        console.log("Error increasing likes");
+        alert("Unable ton increase like count");
+      });
+  };
 
-  return(
+  return (
     <>
-    {/* div to load images or description */}
-    <div className="imgBox" onClick={() => toggle()}>
-      {/* uses click hook to determine whether image or description should be rendered */}
-      {click ? <img src={imageData.path} alt={imageData.description}></img> : <p>{imageData.description}</p>}
+      {/* div to load images or description */}
+      <div className="imgBox" onClick={() => toggle()}>
+        {/* uses click hook to determine whether image or description should be rendered */}
+        {click ? (
+          <img src={imageData.path} alt={imageData.description}></img>
+        ) : (
+          <p>{imageData.description}</p>
+        )}
       </div>
-    {/* conditional rendering to render like count using the proper plurality */}
-    {imageData.likes === 0 ? <p>No one has loves this image yet.</p> : imageData.likes === 1 ? <p>{imageData.likes} person loved this image!</p> :
-    <p>{imageData.likes} people loved this image!</p>}
-    {/* calls incrementLikes when like button is pressed */}
-    <button onClick={() => incrementLikes()}>Love it!</button>
+      {/* conditional rendering to render like count using the proper plurality */}
+      <div className="likeBox">
+        {imageData.likes === 0 ? (
+          <p>No one has loves this image yet.</p>
+        ) : imageData.likes === 1 ? (
+          <p>{imageData.likes} person loved this image!</p>
+        ) : (
+          <p>{imageData.likes} people loved this image!</p>
+        )}
+        {/* calls incrementLikes when like button is pressed */}
+        <button onClick={() => incrementLikes()}>Love it!</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default GalleryItem
+export default GalleryItem;
